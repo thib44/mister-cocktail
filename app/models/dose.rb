6e3c4b4f66d2907ee13139486e452596d4dcf17c
thibault finot <thibault.finot@gmail.com>
2015-10-29 12:32:50 +0100

    create the controller

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,6 @@
 class Dose < ActiveRecord::Base
   belongs_to :cocktail
   belongs_to :ingredient
+  validates :description, :cocktail, :ingredient, presence: true
+  validates_uniqueness_of :cocktail, :scope => [:ingredient]
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,5 @@
 class Ingredient < ActiveRecord::Base
+  has_many :doses
+  before_destroy :check_for_cocktails
+  validates :name, presence: true, uniqueness: true
 end
